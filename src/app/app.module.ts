6cc10b4d231806a2cefefa1a64462f77d4f4e828
Patricia Maldonado <patriciamaldonado410@gmail.com>
2022-11-03 16:23:08 +0100

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+// Tarea:
+// Crear un módulo llamado ContadorModule
+// declaraciones, exportaciones
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
